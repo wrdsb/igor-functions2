1d2008ebb06a078355a587af277b256c0bdbe4e8
James Schumann <jschumann@gmail.com>
2019-03-18 13:35:59 -0400

    rework to use human friendly id slugs

--- calendar-memberships-actual-now-store/index.js
+++ calendar-memberships-actual-now-store/index.js
@@ -1,12 +1,13 @@
 module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
 
-    context.bindings.calendarMembershipsNow = JSON.stringify(req.body);
+    context.bindings.calendarMembershipsNow = JSON.stringify(req.body.memberships);
 
     context.res = {
         status: 200,
-        body: req.body
+        body: req.body.memberships
     };
+
     context.log(context.res);
     context.done(null, context.res);
 }
