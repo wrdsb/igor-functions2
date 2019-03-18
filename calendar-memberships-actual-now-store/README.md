2d756ea544d4a3d7d4d58fe5ebbec5da946bae11
James Schumann <jschumann@gmail.com>
2019-03-18 10:15:44 -0400

    add calendar-memberships-actual-now-store

---
+++ calendar-memberships-actual-now-store/README.md
@@ -0,0 +1,2 @@
+# Groups Memberships Actual Now Store
+Updates blob file with group membership supplied via HTTP request.
---
+++ calendar-memberships-actual-now-store/index.js
@@ -0,0 +1,12 @@
+module.exports = function (context, req) {
+    var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+
+    context.bindings.calendarMembershipsNow = JSON.stringify(req.body);
+
+    context.res = {
+        status: 200,
+        body: req.body
+    };
+    context.log(context.res);
+    context.done(null, context.res);
+}
