62806a5d8c178d4f0569e9a678187ea8158c67ee
James Schumann <james@schumann.ca>
2020-01-21 14:48:02 -0500

    groups-memberships-extract-admins working

--- groups-memberships-extract-admins/index.js
+++ groups-memberships-extract-admins/index.js
@@ -1,12 +1,31 @@
 module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
+    var owners = [];
+    var managers = [];
 
-    var memberships = context.bindings.groupMembershipsNow;
+    var blob = context.bindings.groupMembershipsNow;
+    var memberships = blob.actual;
+    var membershipsArray = Object.getOwnPropertyNames(memberships);
+
+    membershipsArray.forEach(function(email) {
+        if (memberships[email]['role'] == 'OWNER') {
+            owners.push(email);
+        }
+        if (memberships[email]['role'] == 'MANAGER') {
+            managers.push(email);
+        }
+    });
+
+    var res = {
+        "email": req.body.group,
+        "owners": owners,
+        "managers": managers
+    }
 
     context.res = {
         status: 200,
-        body: memberships
+        body: res
     };
-    context.log(memberships);
-    context.done(null, memberships);
+    context.log(res);
+    context.done(null);
 }
