34b60a85ab7bf6e184c898588752e616d14a3846
James Schumann <james@schumann.ca>
2020-01-23 07:46:12 -0500

    calc studentOwned, studentManaged, staffOwned, staffManaged

--- groups-memberships-extract-admins/index.js
+++ groups-memberships-extract-admins/index.js
@@ -3,6 +3,11 @@ module.exports = function (context, req) {
     var owners = [];
     var managers = [];
 
+    var studentOwned = false;
+    var staffOwned = false;
+    var studentManaged = false;
+    var staffManaged = false;
+
     var blob = context.bindings.groupMembershipsNow;
     var memberships = blob.actual;
     var membershipsArray = Object.getOwnPropertyNames(memberships);
@@ -10,16 +15,30 @@ module.exports = function (context, req) {
     membershipsArray.forEach(function(email) {
         if (memberships[email]['role'] == 'OWNER') {
             owners.push(email);
+            if (/\d/.test(email)) {
+                studentOwned = true;
+            } else {
+                staffOwned = true;
+            }
         }
         if (memberships[email]['role'] == 'MANAGER') {
             managers.push(email);
+            if (/\d/.test(email)) {
+                studentManaged = true;
+            } else {
+                staffManaged = true;
+            }
         }
     });
 
     var res = {
         "email": req.body.group,
         "owners": owners,
-        "managers": managers
+        "managers": managers,
+        "studentOwned": studentOwned,
+        "staffOwned": staffOwned,
+        "studentManaged": studentManaged,
+        "staffManaged": staffManaged,
     }
 
     context.res = {
