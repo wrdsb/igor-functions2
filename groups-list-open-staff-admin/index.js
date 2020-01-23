4bf4adf120e423991f08f17d0b0ff670cd303e58
James Schumann <james@schumann.ca>
2020-01-23 10:29:10 -0500

    groups-list-open-staff-admin working

--- groups-list-open-staff-admin/index.js
+++ groups-list-open-staff-admin/index.js
@@ -1,12 +1,32 @@
 module.exports = function (context, req) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
     var groups = context.bindings.cosmosGroups;
+    var groupAdmins = [];
 
-    var res = groups;
+    groups.forEach(function(group) {
+        var admins = group.owners.concat(group.managers);
+        var adminsString = '';
+
+        admins.forEach(function(admin) {
+            adminsString = adminsString + admin;
+            adminsString = adminsString + ',';
+        });
+        adminsString = adminsString.slice(0, -1);
+
+        groupAdmins.push(
+            {
+                "email": group.email,
+                "name": group.name,
+                "group-admins": adminsString
+            }
+        );
+    });
+
+    var res = groupAdmins;
 
     context.res = {
         status: 200,
-        body: res
+        body: groupAdmins
     };
     context.log(res);
     context.done(null);
